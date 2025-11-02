3f4cd2bad0166141fd19baaf0dad53eea1c8e2c7
muashef <muhammedsherif40@yahoo.com>
2025-11-03 00:37:51 +0100

    fix: fix routing and adjusted layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
-module.exports = {
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-    container: {
-      center: true,
-      padding: '1rem',
-    }
-  },
-  plugins: [],
+export const content = [
+  "./app/**/*.{js,jsx,ts,tsx}",
+  "./components/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {},
+  container: {
+    center: true,
+    padding: '1rem',
+  }
 };
+export const plugins = [];
